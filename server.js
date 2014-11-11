107a20747245e5c6ec39140935f62d7f38a5a662
Matija Stepanic <stepanic.matija@gmail.com>
2014-11-11 01:38:00 +0100

    Connected to MongoDB databese on MongoLab

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ process.env.PWD = process.cwd();
 
 
 var express = require('express');
+var mongoose = require('mongoose');
+var jobModel = require('./models/Job');
 
 var app = express();
 
@@ -10,9 +12,22 @@ app.set('view engine', 'jade');
 
 app.use(express.static(process.env.PWD + '/public'));
 
+app.get('/api/jobs', function(req, res) {
+  mongoose.model('Job').find({}).exec(function(error, collection) {
+    res.send(collection);
+  });
+});
+
 app.get('*', function (req, res) {
   res.render('index');
 });
 
+mongoose.connect('mongodb://demo:demo1234@ds051740.mongolab.com:51740/heroku_app31481344');
+
+var con = mongoose.connection;
+con.once('open', function() {
+  console.log('connected to mongodb sucessfully!');
+  jobModel.seedJobs();
+});
 
 app.listen(process.env.PORT || 3000);
