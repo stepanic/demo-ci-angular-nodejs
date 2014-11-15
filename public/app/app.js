9c18e9b2880110dbb330a2410f5889c2939a8701
Matija Stepanic <stepanic.matija@gmail.com>
2014-11-15 03:40:05 +0100

    Created Job input form and updated Karma to run once

--- public/app/app.js
+++ public/app/app.js
@@ -3,5 +3,11 @@ app = angular.module('app', ['ngResource']);
 
 app.controller('testCtrl', ['$scope', '$resource', 'jobs', function($scope, $resource, jobs){
   $scope.jobs = $resource('/api/jobs').query();
-  jobs.save({title:'My testing job title', description:'My testing description'});
+
+  $scope.submit = function() {
+    var job = {title: $scope.title, description: $scope.description};
+    console.log(job);
+    jobs.save(job);
+    $scope.jobs.push(job);
+  };
 }])
